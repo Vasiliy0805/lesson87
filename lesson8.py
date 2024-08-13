dfb260ef917f8d55eed6a90a6749eba83969997d
Vasyliy0805 <vasst1991@yandex.ru>
2024-08-13 15:29:10 +0500

    new file:   .venv/pyvenv.cfg new file:   lesson8.py new file:   requirements.txt

---
+++ lesson8.py
@@ -0,0 +1,14 @@
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.types.web_app_info import WebAppInfo
+
+bot = Bot("7287345480:AAHJhiqftTfUlaUY7wKCMUtNfEUUk0jfyfE")
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=["start"])
+async def start(message: types.message):
+    markup = types.ReplyKeyboardMarkup()
+    markup.add(types.KeyboardButton("Открыть веб  страницу", web_app= WebAppInfo(url = "https://www.avito.ru")))
+    await message.answer("привет, ты кто?", reply_markup = markup)
+
+executor.start_polling(dp)
