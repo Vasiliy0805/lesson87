d0c7a7a1df1c58ea6fd7674e5f9905f999903d19
Vasyliy0805 <vasst1991@yandex.ru>
2024-08-13 17:12:13 +0500

    modified:   lesson8.py

--- lesson8.py
+++ lesson8.py
@@ -8,7 +8,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=["start"])
 async def start(message: types.message):
     markup = types.ReplyKeyboardMarkup()
-    markup.add(types.KeyboardButton("Открыть веб  страницу", web_app= WebAppInfo(url = "https://github.com/Vasiliy0805/lesson87/blob/ca57db09a41feafc91aa511ccda716942b976a23/index.html")))
+    markup.add(types.KeyboardButton("Открыть веб  страницу", web_app= WebAppInfo(url = "https://index.html")))
     await message.answer("привет, ты кто?", reply_markup = markup)
 
 executor.start_polling(dp)
